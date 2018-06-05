4eae10adab313dd51e4c9134268d39aa5c25f388
adamtowers <hello@adamtowers.design>
2018-06-05 10:49:26 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+    text: ''
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      text: this.state.text
+    })
+
+    this.setState({
+      text: ''
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Add restaurant: </label>
+          <input
+            type='text'
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.text} />
+          <input type='submit' />
+        </form>
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,14 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    console.log(this.props.store.getState().reviews)
+    const reviews = this.props.store.getState().reviews.filter(review => review.restaurantId === this.props.restaurantId)
+    const reviewComponents = reviews.map((review) => {
+        return <Review key={review.id} review={review} store={this.props.store} />
+    })
     return (
       <ul>
-        Reviews
+        {reviewComponents}
       </ul>
     );
   }
